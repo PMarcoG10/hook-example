ee400358d6559ca02c2f2ee54c94c492a96630b5
PMarcoG10 <marcog10@csu.fullerton.edu>
2023-12-06 10:20:09 -0800

    updated format & comments added

--- recieve.py
+++ recieve.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python
 import pika, sys, os
 
+
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    # connecting to the RabbbitMQ server
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    # making sure that the queue exists
+    channel.queue_declare(queue="hello")
 
+    # when receiving a messgae this function is called by the Pika library
+    # this will also print the contents of the message on the terminal
     def callback(ch, method, properties, body):
         print(f" [x] Received {body}")
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    # this function lets RabbitMQ should receive messages from our 'hello' queue
+    channel.basic_consume(queue="hello", on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    # a never ending loop that waits for data and runs callbacks whenever necessary 
+    print(" [*] Waiting for messages. To exit press CTRL+C")
     channel.start_consuming()
 
-if __name__ == '__main__':
+# catches 'keyboardInterrupts during program shutdown' 
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        print("Interrupted")
         try:
             sys.exit(0)
         except SystemExit:
--- send.py
+++ send.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+# establish a connection to RabbitMQ server
+# this insures that we are connected to a broker on our local machine
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+# in order to send a message we must make sure that a recipeint queue exists
+channel.queue_declare(queue="hello")
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+# this contains a string which well be sent to our 'hello' queue
+# the excahnge helps us to specify exacty which queue the message will be sent to
+# our queue name has be specofied in the 'routing_key' parameter
+channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
 print(" [x] Sent 'Hello World!'")
+
+# helps us with the network buffers being flushed
+# and our messages was successfully sent
 connection.close()
